bd13590ecdeac92ad233af7417242b4a2760bdaa
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-09 15:35:50 +0700

    upd

---
+++ config.js
@@ -0,0 +1,7 @@
+const productConfig = {
+    product1: 'https://trnsfx.ru/go/szwy7zkz28',
+    product2: 'https://trnsfx.ru/go/sfxh90caqh',
+    product3: 'https://trnsfx.ru/go/s91tmubia0',
+    product4: 'https://trnsfx.ru/go/svqxcz3vjm'
+};
+
